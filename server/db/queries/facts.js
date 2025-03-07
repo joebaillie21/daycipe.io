37a6ebf4e3721b8adbaf5225c212ddc0ba56d43f
Coray-Bennett <cob3218@rit.edu>
2025-03-06 20:44:56 -0500

    /today route implemented with 404 logic

--- server/db/queries/facts.js
+++ server/db/queries/facts.js
@@ -5,3 +5,8 @@ export const getFacts = async () => {
     const result = await pool.query("SELECT * FROM facts");
     return result.rows;
 };
+
+export const getCurrentFact = async () => {
+    const result = await pool.query("SELECT * FROM facts WHERE facts.date=CURRENT_DATE LIMIT 1");
+    return result.rows[0];
+}
