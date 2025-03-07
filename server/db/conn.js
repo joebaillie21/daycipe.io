a24a58f70930564b79e92e4e2516f41a481d1ce8
Coray-Bennett <cob3218@rit.edu>
2025-03-06 20:31:52 -0500

    schemaInit implemented

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,11 +1,25 @@
 // server/db/pool.js
 import pkg from 'pg';
+import path from 'path';
+import fs from 'fs';
 const { Pool } = pkg;
 
 export const pool = new Pool({
   user: "postgres",         // Your PostgreSQL username
   host: "localhost",        // Your PostgreSQL host
-  database: "daycipie",     // Your PostgreSQL database
-  password: "",             // Your PostgreSQL password
+  database: "daycipe",      // Your PostgreSQL database
+  password: "password",     // Your PostgreSQL password
   port: 5432                // Default PostgreSQL port
 });
+
+//Initialize DB schema
+const schemaPath = path.resolve('db/schema.sql');
+const schemaQuery = fs.readFileSync(schemaPath, 'utf-8');
+
+export const schemaInit = async () => {
+    try {
+        pool.query(schemaQuery);
+    } catch (err) {
+        console.log("Could not initialize database schema: ", err);
+    }
+}
--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,49 +1,51 @@
--- Table for facts
-CREATE TYPE IF NOT EXISTS fact_category AS ENUM ('math', 'physics', 'bio', 'compsci', 'chem');
+DROP TABLE IF EXISTS reports;
+DROP TYPE IF EXISTS report_type;
+DROP TABLE IF EXISTS jokes;
+DROP TABLE IF EXISTS recipes;
+DROP TYPE IF EXISTS recipe_category;
+DROP TABLE IF EXISTS facts;
+DROP TYPE IF EXISTS fact_category;
 
-CREATE TABLE IF NOT EXISTS facts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+-- Table for facts
+CREATE TYPE fact_category AS ENUM ('math', 'physics', 'bio', 'compsci', 'chem');
+
+CREATE TABLE  facts (
+    id SERIAL PRIMARY KEY,
     date DATE NOT NULL,
-    content TEXT NOT NULL,
-    source TEXT,
+    content VARCHAR(1000) NOT NULL,
+    source VARCHAR(200),
     score INT DEFAULT 0,
     category fact_category NOT NULL,
     is_shown BOOLEAN DEFAULT TRUE
 );
 
 -- Table for recipes
-CREATE TYPE IF NOT EXISTS recipe_category AS ENUM ('veganism', 'vegetarianism', 'lactose_intolerance', 'gluten_intolerance', 'kosher');
+CREATE TYPE recipe_category AS ENUM ('veganism', 'vegetarianism', 'lactose_intolerance', 'gluten_intolerance', 'kosher');
 
-CREATE TABLE IF NOT EXISTS recipes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE recipes (
+    id SERIAL PRIMARY KEY,
     date DATE NOT NULL,
-    content TEXT NOT NULL,
+    content VARCHAR(2000) NOT NULL,
     score INT DEFAULT 0,
     category recipe_category NOT NULL,
     is_shown BOOLEAN DEFAULT TRUE
 );
 
 -- Table for jokes
-CREATE TABLE IF NOT EXISTS jokes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE jokes (
+    id SERIAL PRIMARY KEY,
     date DATE NOT NULL,
-    content TEXT NOT NULL,
+    content VARCHAR(500) NOT NULL,
     score INT DEFAULT 0,
     is_shown BOOLEAN DEFAULT TRUE
 );
 
 -- Table for reports
-CREATE TYPE IF NOT EXISTS report_type AS ENUM ('fact', 'recipe', 'joke');
+CREATE TYPE report_type AS ENUM ('fact', 'recipe', 'joke');
 
-CREATE TABLE IF NOT EXISTS reports (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE reports (
+    id SERIAL PRIMARY KEY,
     type_of_reported_content report_type NOT NULL,
     reported_content_id INT NOT NULL,
-    substance_of_report TEXT NOT NULL,
-    CONSTRAINT fk_reported_fact FOREIGN KEY (reported_content_id) 
-        REFERENCES facts(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT fk_reported_recipe FOREIGN KEY (reported_content_id)
-        REFERENCES recipes(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT fk_reported_joke FOREIGN KEY (reported_content_id)
-        REFERENCES jokes(id) ON DELETE CASCADE ON UPDATE CASCADE
+    substance_of_report TEXT NOT NULL
 );
